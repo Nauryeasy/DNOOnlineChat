6f8be716f0aa1179fdc31ab00e917f1684ce53bf
Ivan_Karavaev <koertyf@gmail.com>
2023-04-05 18:48:39 +0400

    ...

--- DnoChat/DnoChat/settings.py
+++ DnoChat/DnoChat/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['84.201.187.90']
 
 
 # Application definition
